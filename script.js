5659d2cda07f82c8367f4c07ede7d83317a63d64
Jaquelinlara <lara.jackie09@gmail.com>
2020-12-17 19:32:23 -0500

    header and footer

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerIcon = document.querySelector(".hamburger--container");
-const navbar = document.querySelector(".navbar");
-
-hamburgerIcon.addEventListener("click", function(){
-    navbar.classList.toggle("change")
-})
