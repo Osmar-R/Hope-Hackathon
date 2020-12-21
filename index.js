a6555b500ffa213428ccb14afa86c9d9b9690cdb
ashley-woodton <ashley.woodton@gmail.com>
2020-12-20 23:44:58 -0500

    final ejs

--- index.js
+++ index.js
@@ -2,76 +2,56 @@ const express=require('express')
 const path=require('path')
 const app=express()
 const staticPath=path.join(__dirname,'public')
+const request = require("request");
 const bodyParser = require('body-parser')
 
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(staticPath)) //set 'public' as static folder
 
 
 
-app.get('/',(req,res)=>{ //url root for sending home page
+app.get('/cases',(req,res)=>{ //url root for sending home page
     res.sendFile(staticPath+'/html/index.html')
 })
 
-
-/////////////////////
-
-
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static('public'));
-
-
 app.get('/', function(req,res){
-    res.render('index');
+    res.render('index', { location1: null, location2: null, location3: null, error: null });
 })
 
 app.post('/', (req,res)=>{
     let state = req.body.state;
     state = state.toLowerCase()
-    const statsUrl = `https://api.covidtracking.com/v1/states/${state}/current.json`;
-     console.log(statsUrl)
+    const locationUrl = `https://sheetlabs.com/NCOR/covidtestcentersinUS?state=${state}`;
+        console.log(locationUrl)
 
-    // request(statsUrl, function(err, response, body){
-    //     if(err){
-    //         res.render('index', {weather : null, error : 'This aint it'})
-    //     }
-    //     else{
-    //         let weather = JSON.parse(body)
-    //         if(weather.main == undefined){
-    //             res.render('index', {weather : null, error : 'Memphis isnt a state'})
-    //         } else{
-    //             let weatherText = `Its ${weather.main.temp} degress in ${weather.name}`
-    //             res.render('index', {weather: weatherText, error:null})
-    //         }
-    //     } 
-    // })
-        
+    request(locationUrl, function(err, response, body){
+        if(err) {
+            // res.status(500).send("Internal server error")
+            res.status(500).render('index', {location1 : null, location2: null, location3: null, error : 'Internal server error'})
+        }
+        else{
+            let locations = JSON.parse(body)
+            if(locations[0] == undefined){
+                res.status(400).render('index', {location1 : null,  location2: null, location3: null,error : `${state.toUpperCase()} is not a state`})
+                console.log(`${state.toUpperCase()} is not a state`)
+            } else{
+                let l1 = `${locations[0].centername} @ ${locations[0].address}`
+                let l2 = `${locations[1].centername} @ ${locations[1].address}`
+                let l3 = `${locations[2].centername} @ ${locations[2].address}`
+                res.status(200).render('index', {location1 : l1, location2: l2, location3: l3, error : null})
+                // res.status(200).send(weatherText)
+            }
+        } 
     })
+            
+})
+
+/////////////////////
 
 
-// app.post('/', (req, res) => {
-//         let state = req.body.input
-//         let statsUrl = `https://api.covidtracking.com/v1/states/${state}/current.json`
-//         let locationUrl = `https://sheetlabs.com/NCOR/covidtestcentersinUS?state=${state}`
-
-//         if(state.length !=2 || parseInt(state) == !NaN){
-//             res.status(400).send("Invalid State Abbreviation")
-//             return;
-//         }
-//         request(statsUrl, (err, response, body) =>{
-//             if(err || response.status != 200) {
-//                 res.status(500).send("Internal server error")
-//                 return;
-//             }
-//             // This might need to be turned into JSON using JSON stringify
-//             let responseBody = JSON.stringify(body);
-//             // responseBody['newKey'] = "some extra data not provided by the external api"
-//             console.log(responseBody)
-//             res.status(200).send(responseBody)
-//         })
-//    })
-
-////////////////////////////////////
 
 const PORT=process.env.PORT || 5000
 app.listen(PORT,(()=>{
